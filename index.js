c5e4fec957ec3477b63dd9025aad8ee450bdcbe0
devinvogt2 <vogtdevin@ymail.com>
2021-10-18 21:33:01 -0500

    first

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 let score = 0;
+let numOfHoles = 9;
+const scoreDisplay = document.getElementById('score');
+const holeDisplay = document.getElementById('numOfHoles');
+const holes = document.getElementsByClassName('hole');
+
+setInterval(function() {
+  const randomHoleIndex = Math.floor(Math.random() * holes.length);
+  holes[randomHoleIndex].classList.toggle('mole');
+}, 3000);
+
+const gameArea = document.getElementById('whack-a-mole');
+gameArea.addEventListener('click', function(clickEvent) {
+  if (clickEvent.target.matches('.mole')) {
+    clickEvent.target.classList.remove('mole');
+    score++;
+    numOfHoles++;
+    //some dom to chane the css to add a hole
+    scoreDisplay.innerText = "Score:" + score;
+    holeDisplay.innerText = "Current Holes:" + numOfHoles;
+    addHole()
+  }
+});
+
+function addHole () {
+    let newHole = document.createElement("div");
+
+    newHole.className = "hole";
+
+    let board = document.getElementById("whack-a-mole");
+    board.appendChild(newHole);
+    
+}
